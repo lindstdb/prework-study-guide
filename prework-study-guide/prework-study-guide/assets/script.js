3d8d52ae9101178d3e81707d6a23921301d4a6aa
lindstdb <115056565+lindstdb@users.noreply.github.com>
2022-10-18 20:08:07 -0500

    fixed js code

--- prework-study-guide/prework-study-guide/assets/script.js
+++ prework-study-guide/prework-study-guide/assets/script.js
@@ -1,24 +1,4 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-<<<<<<< HEAD
-
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
-
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
-=======
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
@@ -45,4 +25,3 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
->>>>>>> 1ff0a23e40036e25c7c69f937636f17ae8c24def
